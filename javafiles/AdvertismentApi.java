0e8cf7004e99c9c1cd6f66ae50485afbfdcfaa77
dand101 <100424460+dand101@users.noreply.github.com>
2023-03-15 20:43:01 +0200

    .java

---
+++ javafiles/AdvertismentApi.java
@@ -0,0 +1,13 @@
+import java.util.Vector;
+
+public class AdvertismentApi {
+
+  public String advertisments;
+
+    public Vector  myAdvertisment;
+    public Vector  myProfileInformation;
+
+  public void promoteAdsToUsers() {
+  }
+
+}
---
+++ javafiles/ProfileInformation.java
@@ -0,0 +1,26 @@
+import java.util.Vector;
+
+public class ProfileInformation {
+
+  public Integer nrFriends;
+
+  public Integer nrPosts;
+
+  public String friends;
+
+  public String adsRecommendations;
+
+    public Vector  myClientUser;
+    public Vector  myAdvertismentApi;
+    public Vector  myNetworkInformation;
+
+  public void WatchPosts() {
+  }
+
+  public void SeeAllFriends() {
+  }
+
+  public void AddFriend() {
+  }
+
+}
